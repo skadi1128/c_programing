df72a80401f41566625f69585e886a9c1ea58f4c
skadi1128 <100252721+skadi1128@users.noreply.github.com>
2022-02-23 11:03:31 +0800

    Create variables.c

---
+++ week_2/variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int myPhoneNumber = 4666666666;
+    printf("My favorite number is %d.\n", myPhoneNumber);
+
+    double myHeight = 1.9;
+    printf("My height is %lf", myHeight);
+
+    char myLetter = "j";
+    printf("My letter is %c.\n", myHeight);
+
+    char myName[] = "Usukhbayr";
+    printf("My name is %s.\n", myName);
+    return 0;
+}
