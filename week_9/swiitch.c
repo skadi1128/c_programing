8f86acec4ef4e9b13b428ff13140dca7652c80f8
skadi1128 <100252721+skadi1128@users.noreply.github.com>
2022-04-13 11:12:17 +0800

    Create swiitch.c

---
+++ week_9/swiitch.c
@@ -0,0 +1,20 @@
+char letter = 'a';
+ switch (letter)
+ {
+ case 'a':
+     printf("APPLE\n") ;
+     break;
+ case 'b':
+     printf("banana\n");
+     break;
+case 'c';
+    printf("cat\n");
+    break;
+default:
+    printf("other\n");
+    break;
+ }
+
+ return 0;
+
+ }    
